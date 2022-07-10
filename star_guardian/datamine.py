e87d6dc136f74d110103e389f7f0a2b867b05512
Preston Scibek <psscibek@gmail.com>
2022-07-10 11:19:57 -0400

    adding comment for why we have double url

--- star_guardian/datamine.py
+++ star_guardian/datamine.py
@@ -9,7 +9,7 @@ import argparse
 HASH_PREFIX = "star-guardian-hub-2022" # example is arcane-nlex-hub-2021
 HASH_PREFIX_NUMBER = 17 
 URL = "https://frontpage.na.leagueoflegends.com/en_US/channel/lol/home/event/star-guardian-hub-2022" # example is https://frontpage.na.leagueoflegends.com/en_US/channel/lol/home/event/arcane-nlex-hub-2021
-URL = "https://frontpage.na.leagueoflegends.com/en_US/channel/lol/home/event/star-guardians-marketing-hub-2022#/"
+URL = "https://frontpage.na.leagueoflegends.com/en_US/channel/lol/home/event/star-guardians-marketing-hub-2022#/" # this url is just temp until event is fully released, but we still want to get the marketing assets
 HASHED_KEY_LINES_FILE = "hashed_lines_star_guardian.json" # example is 'lines_arcane.json'
 DEHASHED_KEY_LINES_FILE = "dehashed_lines_star_guardian.json" # example is "attempted_bf.json"
 
