b468b3149467c1db332a58007fe66966dbd9ecc4
Preston Scibek <psscibek@gmail.com>
2022-07-12 10:02:43 -0400

    multithreading

--- generic/get_assets.py
+++ generic/get_assets.py
@@ -24,12 +24,16 @@ for line in js.split("\n"):
             #print(f"{asset_path}{m.group(1)}")
             assets.append(f"{asset_path}{m.group(1)}")
 
-for asset in assets:
-    print(f"downloading asset for {asset}")
-    url = asset
+
+fails = []
+success = []
+import subprocess
+import concurrent.futures
+
+def request_get(url):
+    print(f"downloading asset for {url}")
     path = "assets/" + url.split("assets/")[1].split("?")[0]
     dir_path = "/".join(path.split("/")[0:-1])
-    #print(url)
     r = requests.get(url, stream=True)
     if r.status_code == 200:
         if not os.path.exists(dir_path):
@@ -37,3 +41,22 @@ for asset in assets:
         with open(path, 'wb') as f:
             for chunk in r:
                 f.write(chunk)
+        return True
+    else:
+        print(f"download failed for {url}")
+        return False
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=32) as executor:
+    for url, status in zip(assets, executor.map(request_get, assets)):
+        if status:
+            success.append(url)
+        else:
+            fails.append(url)
+
+with open("failed_downloads.json", "w+") as jfile:
+    json.dump(fails, jfile, indent=4)
+with open("success_downloads.json", "w+") as jfile:
+    json.dump(success, jfile, indent=4)
+
+total_assets = len(assets)
+print(f"Total Assets: {total_assets}\nFails: {len(fails)}\nSuccesses: {len(success)}")
