4d3b280b186c85e6a553cc8f9a12f790a0190bf1
Preston Scibek <psscibek@gmail.com>
2022-07-21 14:23:00 -0400

    updating scenes with week 2

--- star_guardian/sg_website_stuff.py
+++ star_guardian/sg_website_stuff.py
@@ -1,6 +1,8 @@
-import json
-with open("sg_website_events.json", 'r') as jfile:
-    j = json.load(jfile)
+import json, requests
+#with open("sg_website_events.json", 'r') as jfile:
+#    j = json.load(jfile)
+url = 'https://starguardian.riotgames.com/page-data/en-us/timeline/page-data.json'
+j = requests.get(url).json()
 res_l = []
 for res in sorted(j['result']['data']['hotspots']['nodes'], key=lambda x: x['content_metadata']['publish_date']):
     print(res['reference'][0]['title'], res['reference'][0]['label'], res['locked_subtitle'], res['content_metadata']['publish_date'], sep="|")
